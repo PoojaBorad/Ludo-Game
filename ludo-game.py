9f71c800c40bfc7dfb9da6fa833d1b102d9afbf1
PoojaBorad <poojabborad@gmail.com>
2023-09-02 18:29:02 -0700

    Update ludo-game.py

--- ludo-game.py
+++ ludo-game.py
@@ -16,21 +16,31 @@ def move_player(player_position, steps):
     player_position += steps
     if player_position > 51:
         player_position -=52
-    return player_position + steps
+    return player_position
 
 #while loop
 while True:
     input(f"{player1}, press enter to roll the dice: ")
-    result = roll_dice()    
-    print(f"{player1} rolled dice a : {result}")
+    result = roll_dice()   
+    print(f"{player1} rolled a : {result}")
     player1_position = move_player(player1_position, result)
     print(f"{player1} moved from {player1_position - result} to {player1_position} steps.")
 
     input(f"{player2}, press enter to roll the dice: ")
     result = roll_dice()    
-    print(f"{player2} rolled dice a: {result} ")
+    print(f"{player2} rolled a: {result} ")
     player2_position = move_player(player2_position, result) 
     print(f"{player2} moved from {player2_position - result} to {player2_position} steps.")
+
+#Declare Winner
+    if player1_position == 51:
+      print(f"{player1} is a Winner!")
+      break
+    elif player2_position ==51:
+      print(f"{player2} is a Winner!")
+      break
+
+
 
 
 
