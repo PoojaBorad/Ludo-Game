3f4808117fc39cf90dd83235d197129ebc11b75d
PoojaBorad <poojabborad@gmail.com>
2023-09-02 18:13:50 -0700

    Update ludo-game.py

--- ludo-game.py
+++ ludo-game.py
@@ -3,8 +3,8 @@ import random
 #Board size
 board = [" "] * 52
 
-player1 = input("Enter a name of player 1: ")
-player2 = input("Enter a name of player 2: ")
+player1 = input("Enter a name of player 1: ").capitalize()
+player2 = input("Enter a name of player 2: ").capitalize()
 
 player1_position = 0
 player2_position = 0
@@ -16,13 +16,21 @@ def move_player(player_position, steps):
     player_position += steps
     if player_position > 51:
         player_position -=52
-    return player_position
+    return player_position + steps
 
 #while loop
 while True:
     input(f"{player1}, press enter to roll the dice: ")
-    result =roll_dice()    
-    print(f"{player1} rolled a , {result}") 
+    result = roll_dice()    
+    print(f"{player1} rolled dice a : {result}")
+    player1_position = move_player(player1_position, result)
+    print(f"{player1} moved from {player1_position - result} to {player1_position} steps.")
+
+    input(f"{player2}, press enter to roll the dice: ")
+    result = roll_dice()    
+    print(f"{player2} rolled dice a: {result} ")
+    player2_position = move_player(player2_position, result) 
+    print(f"{player2} moved from {player2_position - result} to {player2_position} steps.")
 
 
 
